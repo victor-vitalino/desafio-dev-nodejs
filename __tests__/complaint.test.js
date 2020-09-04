4f70a8b5d1179178150b0041de98227a32124406
Victor Vitalino <victorvitalino237@gmail.com>
2020-09-04 14:20:04 -0300

    test: added test to request external api

--- __tests__/complaint.test.js
+++ __tests__/complaint.test.js
@@ -2,12 +2,63 @@ import request from 'supertest';
 import app from '../src/app';
 
 describe('Complaint', () => {
-    it('returns error if latitude and longitude are not sent', async () => {
-        const response = await request(app).post('/v1/denuncias').send({
-            latitude: '',
-            longitude: '93232',
-        });
+    it('returns error if data from request.body are not sent', async () => {
+        const response = await request(app)
+            .post('/v1/denuncias')
+            .send({
+                latitude: null,
+                longitude: -36.76422,
+                denunciante: {
+                    nome: 'José de Oliveira',
+                    cpf: '95761638037',
+                },
+                denuncia: {
+                    titulo: 'Esgoto a céu aberto',
+                    descricao:
+                        'Existe um esgoto a céu aberto nesta localidade.',
+                },
+            });
 
         expect(response.status).toBe(400);
+    });
+
+    it('returns success after sending the correct data in the request body', async () => {
+        const response = await request(app)
+            .post('/v1/denuncias')
+            .send({
+                latitude: -9.6329144,
+                longitude: -35.7297573,
+                denunciante: {
+                    nome: 'José de Oliveira',
+                    cpf: '95761638037',
+                },
+                denuncia: {
+                    titulo: 'Esgoto a céu aberto',
+                    descricao:
+                        'Existe um esgoto a céu aberto nesta localidade.',
+                },
+            });
+
+        expect(response.status).toBe(200);
+    });
+
+    it('returns error if external api dont found the address', async () => {
+        const response = await request(app)
+            .post('/v1/denuncias')
+            .send({
+                latitude: -9,
+                longitude: -3,
+                denunciante: {
+                    nome: 'José de Oliveira',
+                    cpf: '95761638037',
+                },
+                denuncia: {
+                    titulo: 'Esgoto a céu aberto',
+                    descricao:
+                        'Existe um esgoto a céu aberto nesta localidade.',
+                },
+            });
+
+        expect(response.status).toBe(401);
     });
 });
